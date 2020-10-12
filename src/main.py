b1ae6740c2724abc6ae13273eac81532374401f1
david.hresko <hreskodavid57@gmail.com>
2020-10-12 23:14:07 +0200

    5x5 gen kernel

--- src/main.py
+++ src/main.py
@@ -3,4 +3,4 @@ from src.gan.gan import GAN
 if __name__ == '__main__':
     gan = GAN()
 
-    gan.train(epochs=5000, batch_size=256, sample_interval=200)
+    gan.train(epochs=7000, batch_size=100, sample_interval=200)
